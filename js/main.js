a2446af1411b48b2c88ffdd3e4461992618fa540
Mohammad Shohel Rana <47520471+shohelranabadc@users.noreply.github.com>
2023-11-12 21:59:13 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true,
+  'alwaysShowNavOnTouchDevices': true,
+  'fitImagesInViewport': true,
+  'fadeDuration': 600,
+  'fitImagesInViewport': true,
+  'imageFadeDuration': true,
+  'showImageNumberLabel': true,
+  'wrapAround': true,
+  'resizeDuration': 700 
+});
